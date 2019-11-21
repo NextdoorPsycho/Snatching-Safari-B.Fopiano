be38992b446d5fe7479e14ada83370ef1deb8d55
NextdoorPsycho <brianneumann630@gmail.com>
2019-11-21 11:14:33 -0800

    Rebase WORKING
    Creds saved now

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
- //Brian Fopiano
+
+// # Application Bootup
+// boots up the application
+
+// defines that javascript should be executed in 'strict mode' (ECMAScript 5)
 'use strict';
+
 // # Initialise CLI
 // imports the Command Line Interface (CLI) in order to make the application work
 module.exports = require('./lib/cli/index.js');
-process.on('SIGINT', function() {
-    process.exit();
+
+// # Closing Application
+// make sure the manager gets stopped
+process.on( 'SIGINT', function() {
+  process.exit( );
 });
